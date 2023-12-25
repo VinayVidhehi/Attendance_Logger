8fc428696a5b11aeff888730f8b58f7a08666394
VinayVidhehi <vinayvidhehi@gmail.com>
2023-12-25 15:00:39 +0530

    simple post route

--- server/app.js
+++ server/app.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 7700;
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Define a sample route
+app.get('/', (req, res) => {
+  res.send('Hello, this is your Express server with CORS!');
+});
+
+app.post('/test', (req, res) => {
+    const value = req.body;
+    console.log("the received data is ", value);
+    res.status(200).send({message:"post successfull", key:1})
+})
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
