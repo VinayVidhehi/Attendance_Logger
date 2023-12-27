76ec3cdcdc37026ad4211b16a509a0ce99c212c4
VinayVidhehi <vinayvidhehi@gmail.com>
2023-12-27 20:23:37 +0530

    ocr added nothing else

--- server/app.js
+++ server/app.js
@@ -1,24 +1,62 @@
 const express = require('express');
+const mysql = require('mysql2');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
-const port = 7700;
+const PORT = 7700;
 
-// Enable CORS for all routes
+// Enable CORS
 app.use(cors());
 
-// Define a sample route
-app.get('/', (req, res) => {
-  res.send('Hello, this is your Express server with CORS!');
+// Parse incoming request bodies in a middleware before your handlers
+app.use(bodyParser.json());
+
+// MySQL Connection
+const connection = mysql.createConnection({
+  host: 'mysql-36e54173-mulberrydatabase.a.aivencloud.com',
+  port: 24400,
+  user: 'avnadmin',
+  password: 'AVNS_PXPi8DwKVq_UOKMdt1m',
+  database: 'defaultdb',
 });
 
-app.post('/test', (req, res) => {
-   // const value = req.body.key;
-    console.log("Data received");
-    res.status(200).send({message:"post successfull", key:1})
-})
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
+});
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Define the POST endpoint for ESP32 requests
+app.post('/update-attendance', (req, res) => {
+  // Extract the string containing USNs from the request payload
+  const usnString = req.body.usnString;
+
+  // Split the string into an array of USNs
+  const usnArray = usnString.split(',');
+
+  // Update the attendance table in the database
+  const currentDate = new Date().toISOString().split('T')[0]; // Get current date in 'YYYY-MM-DD' format
+
+  // Use a prepared statement to update the database
+  const updateQuery = 'UPDATE attendance SET status = 1, date = ? WHERE usn = ?';
+
+  usnArray.forEach((usn) => {
+    connection.query(updateQuery, [currentDate, usn], (err, results) => {
+      if (err) {
+        console.error('Error updating attendance:', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+      console.log(`Attendance updated for USN: ${usn}`);
+    });
+  });
+
+  res.status(200).send('Attendance updated successfully');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
