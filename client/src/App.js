a3c2d6c7f8fe5d5138ab80df726f248c3541aea2
SpoorthiVarumbudi <spoorthivarumbudi@gmail.com>
2023-12-28 12:41:30 +0530

    components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,22 @@
 // App.js
 
-import React, { useState } from 'react';
-import performOCR from './Components/OCRUtil';
+import React from 'react';
+import Body from './Components/Body/Body';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import About from './Components/About/About';
+
+import {Routes, Route} from 'react-router-dom';
 
 const App = () => {
-  const [image, setImage] = useState(null);
-  const [extractedText, setExtractedText] = useState('');
-
-  const handleImageChange = (event) => {
-    const file = event.target.files[0];
-    setImage(file);
-  };
-
-  const handleOCR = async () => {
-    if (!image) {
-      console.error('No image selected');
-      console.log("hi");
-      return;
-    }
-
-    try {
-      const text = await performOCR(image);
-      setExtractedText(text);
-    } catch (error) {
-      console.error('Error performing OCR:', error);
-    }
-  };
-
   return (
     <div>
-      <input type="file" accept="image/*" onChange={handleImageChange} />
-      <button onClick={handleOCR}>Perform OCR</button>
-      <div>
-        <h2>Extracted Text:</h2>
-        <p>{extractedText}</p>
-      </div>
+      <Header />
+      <Body />
+      <About />
+      <Footer />
     </div>
-  );
+  )
 };
 
 export default App;
---
+++ client/src/Components/Body/Body.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import performOCR from '../OCRUtil';
+
+const Body = () => {
+    const [image, setImage] = useState(null);
+    const [extractedText, setExtractedText] = useState('');
+  
+    const handleImageChange = (event) => {
+      const file = event.target.files[0];
+      setImage(file);
+    };
+  
+    const handleOCR = async () => {
+      if (!image) {
+        console.error('No image selected');
+        console.log("hi");
+        return;
+      }
+  
+      try {
+        const text = await performOCR(image);
+        setExtractedText(text);
+      } catch (error) {
+        console.error('Error performing OCR:', error);
+      }
+    };
+  
+    return (
+      <div>
+        <input type="file" accept="image/*" onChange={handleImageChange} />
+        <button onClick={handleOCR}>Perform OCR</button>
+        <div>
+          <h2>Extracted Text:</h2>
+          <p>{extractedText}</p>
+        </div>
+      </div>
+    );
+}
+
+export default Body
